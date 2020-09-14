6a0466e380152329f8040006bc8b02a078d7bde2
osaidalaraj <osaid_alaraj@hotmail.com>
2020-09-14 16:41:44 +0300

    admin and roles are added

---
+++ GroceryHub/Controllers/AdminController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroceryHub.Data;
+using GroceryHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryHub.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private IPasswordHasher<AppUser> passwordHasher;
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(UserManager<AppUser> usrMgr, IPasswordHasher<AppUser> passwordHash, ApplicationDbContext context)
+        {
+            userManager = usrMgr;
+            passwordHasher = passwordHash;
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexAsync(int pageNumber = 1)
+        {
+            //return View();
+
+            return View(await PaginatedList<AppUser>.CreateAsync(userManager.Users, pageNumber, 5));
+
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    UserName = user.Email,
+                    Email = user.Email,
+                };
+                appUser.EmailConfirmed = true;
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email, string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                {
+                    user.Email = email;
+                    user.UserName = email;
+                }
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                Cart cart = _context.Cart.Where(i => i.AppUserID == id).FirstOrDefault();
+                _context.Cart.Remove(cart);
+                _context.SaveChanges();
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+        }
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+    }
+}
