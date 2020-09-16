aa8c917bfea639ffaa9ec7d4e952d44c0397205e
osaidalaraj <osaid_alaraj@hotmail.com>
2020-09-16 15:19:31 +0300

    Database Edit

---
+++ GroceryHub/Data/Migrations/20200916115503_DBError.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroceryHub.Data.Migrations
+{
+    public partial class DBError : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
